f6b7ee3c735265663f3d60b1e33c9cf2f847266f
Tanmaya Samantray <tanmay20042001@gmail.com>
2024-06-06 13:18:33 +0530

    Auth route and Signup endpoint

---
+++ backend/db/connectToMongoDB.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import {config} from "dotenv"
+
+config();
+const connectToMogoDB = async () =>{
+    try {
+        await mongoose.connect(process.env.MONGO_DB_URI);
+        console.log("Connected to MongoDB")
+    } catch (error) {
+        console.log("Error connecting to database", error.message);
+    }
+};
+
+export default connectToMogoDB;
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
 import express from "express";
 import { config } from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import connectToMogoDB from "./db/connectToMongoDB.js";
 
 const app = express();
 config();
 const PORT = process.env.PORT || 5000
 
-app.get("/", (req, res) => {
-    // root route http://localhost:5000/
-    res.send("Hello World");
-});
+// app.get("/", (req, res) => {
+//     // root route http://localhost:8000/
+//     res.send("Hello World");
+// });
+app.use(express.json()); // To parse incoming request with json payloads
+app.use("/api/auth", authRoutes);                      
 
-app.listen(PORT, () => console.log(`Sever active on PORT ${PORT}.`));
+app.listen(PORT, () => {
+    connectToMogoDB();
+    console.log(`Sever active on PORT ${PORT}.`);
+});
