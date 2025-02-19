4dee7cd00286582a40f9eac4ecb64d67558c2b56
tanmayasamantray <tanmay20042001@gmail.com>
2025-02-19 17:44:40 +0530

    Add Zustand for state management and implement conversation features

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,14 +1,21 @@
+import useGetConversation from "../../hooks/useGetConversation";
+import { getRandomEmoji } from "../../utils/emojis";
 import Conversation from "./Conversation"
 
 const Conversations = () => {
+  const {loading, conversations} = useGetConversation();
+  console.log("CONVERSATIONS: ", conversations)
   return (
     <div className='py-2 flex flex-col overflow-auto'>
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
+      {conversations.map((conversation, Idx) => (
+        <Conversation 
+        key={conversation._id} 
+        conversation={conversation} 
+        emoji = {getRandomEmoji()}
+        lastIdx = {Idx === conversations.length -1}
+        />
+      ))}
+      {loading ? <span className='loading loading-spinner mx-auto'></span> : null}
     </div>
   )
 }
