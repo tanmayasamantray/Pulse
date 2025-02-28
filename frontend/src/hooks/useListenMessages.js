4f170a752829586bc431f6fa68c86a838d8be3ba
tanmayasamantray <tanmay20042001@gmail.com>
2025-02-28 12:31:21 +0530

    hotfix 4

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -2,31 +2,19 @@ import { useEffect } from "react";
 import { useSocketContext } from "../context/SocketContext";
 import useConversation from "../zustand/useConversation";
 import notificationSound from "../assets/sounds/notification.mp3";
-import { decryptMessage } from "../utils/encryption";
-import { useAuthContext } from "../context/AuthContext";
+import { AuthContext } from "../context/AuthContext";
 
 const useListenMessages = () => {
     const { socket } = useSocketContext();
     const { setMessages } = useConversation();
-    const { authUser } = useAuthContext();
+    const {authUser} = AuthContext()
 
     useEffect(() => {
         const handleNewMessage = (newMessage) => {
             newMessage.shouldShake = true;
             const sound = new Audio(notificationSound);
             sound.play();
-
-            // Decrypt the message before adding it to the messages array
-            const decryptedMessage = {
-                ...newMessage,
-                message: decryptMessage(
-                    newMessage.message,
-                    newMessage.senderId,
-                    authUser._id
-                )
-            };
-            
-            setMessages(prev => [...prev, decryptedMessage]);
+            setMessages(prev => [...prev, newMessage]);
         };
 
         socket?.on("newMessage", handleNewMessage);
