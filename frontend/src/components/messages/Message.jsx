a22c43bf7dab12dfe6db2abdcab5001bc7a83f85
tanmayasamantray <tanmay20042001@gmail.com>
2025-02-28 17:15:37 +0530

    Revert "hotfix 3"
    This reverts commit 9c615dbfeb2ef149cc02c36152a4b256de85f8a1.

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,7 +1,3 @@
-import { useAuthContext } from "../../context/AuthContext";
-import { extractTime } from "../../utils/extractTime";
-import useConversation from "../../zustand/useConversation";
-
 const Message = ({ message }) => {
     const { authUser } = useAuthContext();
     const { selectedConversation } = useConversation();
@@ -28,5 +24,3 @@ const Message = ({ message }) => {
         </div>
     );
 };
-
-export default Message; 
