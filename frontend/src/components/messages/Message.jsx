9c615dbfeb2ef149cc02c36152a4b256de85f8a1
tanmayasamantray <tanmay20042001@gmail.com>
2025-02-28 12:03:36 +0530

    hotfix 3

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,3 +1,7 @@
+import { useAuthContext } from "../../context/AuthContext";
+import { extractTime } from "../../utils/extractTime";
+import useConversation from "../../zustand/useConversation";
+
 const Message = ({ message }) => {
     const { authUser } = useAuthContext();
     const { selectedConversation } = useConversation();
@@ -24,3 +28,5 @@ const Message = ({ message }) => {
         </div>
     );
 };
+
+export default Message; 
